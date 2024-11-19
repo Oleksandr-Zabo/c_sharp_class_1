e3d163a17064bd00b70529b40d5f49303dbd757b
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-19 20:13:52 +0200

    ex. 4 lab had done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 ﻿namespace c_sharp_class_1
-{//dev
+{//lab ex- 4
+    using c_sharp_class_1.Entity;
     internal class Program
     {
+        
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var city = new Entity.City("New York", "USA", 8_500_000, 212);
+            city[0] = "Manhattan";
+            city[1] = "Brooklyn";
+            city[2] = "Queens";
+            System.Console.WriteLine(city);
         }
     }
 }
