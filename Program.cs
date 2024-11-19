a712d02052947877988b7b94b558b727c6aba6fe
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-19 19:21:40 +0200

    ex. 2 lab had done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
-﻿namespace c_sharp_class_1
-{//dev
-    internal class Program
+﻿public class c_sharp_class_1
+{//lab ex-2
+    public static void Main()
     {
-        static void Main()
+        Console.Write("Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        bool isFibonacci = IsFibonacci(number);
+        Console.WriteLine($"{number} is Fibonacci: {isFibonacci}");
+    }
+
+    public static bool IsFibonacci(int number)
+    {
+        if (number < 0) return false;
+
+        int a = 0;
+        int b = 1;
+
+        while (b < number)
         {
-            Console.WriteLine("Hello, World!");
+            int temp = b;
+            b = a + b;
+            a = temp;
         }
+
+        return b == number || a == number;
     }
 }
+
 
 
