d828ea07f429975c8a1da55cce6484dd75df236b
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-22 18:50:13 +0200

    ex. 1 hw had done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace c_sharp_class_1
-{//dev
+{//hw ex-1
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Enter lenth of square: ");
+                string? input = Console.ReadLine();
+                if (input == null)
+                {
+                    throw new Exception("input is null");
+                }
+
+                int n = Convert.ToInt32(input);
+                if (n < 0)
+                {
+                    throw new Exception("input is negative");
+                }
+
+                Console.WriteLine("Enter char: ");
+                input = Console.ReadLine();
+                if (input == null || input.Length != 1)
+                {
+                    throw new Exception("input is null or not char");
+                }
+                char c  = Convert.ToChar(input);
+                print_square(n, c);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void print_square(int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(c);
+                Console.WriteLine();
+            }
         }
     }
 }
