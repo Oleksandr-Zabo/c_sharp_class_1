6de2475a5f5e0aca36cb1b573cfeefd23c577736
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-22 18:54:22 +0200

    ex. 2 hw had done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,51 @@
 ﻿namespace c_sharp_class_1
-{//dev
+{//hw ex-2
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Enter number: ");
+                string? input = Console.ReadLine();
+                if (input == null)
+                {
+                    throw new Exception("input is null");
+                }
+
+                int number = Convert.ToInt32(input);
+                if (number < 0)
+                {
+                    throw new Exception("input is negative");
+                }
+
+                is_palindrome(number);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void is_palindrome(int number) {
+            int n = number;
+            int reverse = 0;
+            while (n > 0)
+            {
+                int remainder = n % 10;
+                reverse = reverse * 10 + remainder;
+                n /= 10;
+            }
+            if (number == reverse)
+            {
+                Console.WriteLine("Number is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("Number is not a palindrome.");
+            }
         }
     }
 }
